8a4c1287c4b3153524a4081b5e8272e137f55116
Brayan <brayantandap@gmail.com>
2024-02-20 11:53:37 -0500

    updated > index.js

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ import { PORT } from './src/config.js';
 app.listen(PORT);
 console.log('Live serve on: ' + 'http://localhost:' + PORT)
 
+//Ruta de entrada a la API
+app.get('/', (req, res) => {
+  res.send('Connected to API Ecommerce')
+})
