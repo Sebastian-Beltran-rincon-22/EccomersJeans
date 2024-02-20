6c7b4ce3820b6caa33ab742ed07835756d42f9e9
Daniel <Dannyking07@outlook.es>
2024-02-20 16:43:27 -0500

    feat publication -> controllers

---
+++ src/controller/publication.js
@@ -0,0 +1,90 @@
+import Publication from "../schema/Publicacion";
+
+//Create
+
+export const ge = async (req, res) =>{
+
+  const {title, description, price, img, size, colors} = req.body;
+  if (!title || !description || !price || !img){
+    return res.status(400).json({ msg: 'Todos los campos son obligatorios' });}
+
+
+  try {
+    const newPublication = new Publication ({
+      title,
+      description,
+      price,
+      img,
+      size,
+      colors
+    });
+
+    const publicacionSaved = await newPublication.save();
+
+    res.status(201).json(publicacionSaved);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error)
+  }
+};
+
+//Get
+export const getPublication = async (req, res) => {
+  try {
+    const Publicacions = await Publication.find({})
+    return res.json(Publicacions);
+  }
+  catch (error) {
+    return res.status(500).json({ msg:error.message })
+  }
+}
+
+//GetForId
+
+export const getPublicationById = async (req, res) => {
+  try{
+    const { id } = req.params;
+    res.status(200).json(id)
+  }catch (error){
+    return res.status(500).json({ msg: error.message})
+  }
+}
+
+//Update
+
+export const updatePublicationById = async (req, res) =>{
+  try {
+    const { id } = req.params
+    const { title, description, price, img, size, colors} = req.body
+    const existingPublication = await Publication.findById(id);
+    if (!existingPublication) {
+      return res.status(404).json({ message: "Publication not found"})
+    }
+
+    await Publication.findByIdAndUpdate(id, {
+      title: title,
+      description: description,
+      price: price,
+      img: img,
+      size: size,
+      colors: colors
+    });
+
+    const updatePublication = Publication.findById(id);
+    return res.json(updatePublication);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+//Delete
+export const deletePublicationById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Publication.findByIdAndDelete(id);
+    res.json({ msg: "Publication deleted" })
+  } catch (error) {
+    return res.status(500).json({ msg: error.message })
+}
+
+};
