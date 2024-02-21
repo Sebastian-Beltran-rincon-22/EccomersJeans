ea2f67119fa30658c7b4ea1a1ef6b86e7f6be8cb
Daniel <Dannyking07@outlook.es>
2024-02-21 13:30:26 -0500

    update publication -> getforid and update

--- src/controller/publication.js
+++ src/controller/publication.js
@@ -1,8 +1,8 @@
-import Publication from "../schema/Publicacion";
+import Publication from "../schema/Publicacion.js";
 
 //Create
 
-export const ge = async (req, res) =>{
+export const createPublication = async (req, res) =>{
 
   const {title, description, price, img, size, colors} = req.body;
   if (!title || !description || !price || !img){
@@ -37,18 +37,19 @@ export const getPublication = async (req, res) => {
   catch (error) {
     return res.status(500).json({ msg:error.message })
   }
-}
+};
 
 //GetForId
 
 export const getPublicationById = async (req, res) => {
   try{
     const { id } = req.params;
-    res.status(200).json(id)
+    const Publications = await Publication.findById(id);
+    res.status(200).json(Publications)
   }catch (error){
     return res.status(500).json({ msg: error.message})
   }
-}
+};
 
 //Update
 
@@ -56,6 +57,7 @@ export const updatePublicationById = async (req, res) =>{
   try {
     const { id } = req.params
     const { title, description, price, img, size, colors} = req.body
+
     const existingPublication = await Publication.findById(id);
     if (!existingPublication) {
       return res.status(404).json({ message: "Publication not found"})
@@ -67,15 +69,15 @@ export const updatePublicationById = async (req, res) =>{
       price: price,
       img: img,
       size: size,
-      colors: colors
+      colors: colors,
     });
 
-    const updatePublication = Publication.findById(id);
+    const updatePublication = await Publication.findById(id);
     return res.json(updatePublication);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
-}
+};
 
 //Delete
 export const deletePublicationById = async (req, res) => {
