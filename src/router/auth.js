a691bdb307e10dba55f90f3cbb8582aade2ab6c8
Daniel <Dannyking07@outlook.es>
2024-02-22 12:20:44 -0500

    modifique router

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,10 +1,19 @@
 import express from 'express'
-import controllerAuth from '../controller/auth.js';
+import {signinHandler, signup} from '../controller/auth.js';
 const router = express.Router()
 // const authJwt = require('../middlewares/authJwt')
 // const validateRoles = require('../middlewares/verifyRole')
 
+router.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
 
-router.post('/signup', controllerAuth.signup)
+router.post('/signup', signup)
+
+router.post('/signin', signinHandler)
 
 export default router;
