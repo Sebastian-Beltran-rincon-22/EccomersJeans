cc94238117daadb1c0418689bcca5e940bb65a58
Daniel <Dannyking07@outlook.es>
2024-02-26 11:03:38 -0500

    modifique [auth -> delete comment line]

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,10 +1,7 @@
 import express from 'express'
-// import {signinHandler, signup} from '../controller/auth.js';
 import controllerAuth from '../controller/auth.js';
 
 const router = express.Router()
-// const authJwt = require('../middlewares/authJwt')
-// const validateRoles = require('../middlewares/verifyRole')
 
 router.use((req, res, next) => {
   res.header(
